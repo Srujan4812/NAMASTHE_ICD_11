b9a14fb0ca0f309adf4e3035fbcf4b3b69af834b
Srujan K R <srujankr.cs23@bmsce.ac.in>
2026-01-04 13:33:53 +0530

    Remove all remaining database dependencies, make truly CSV-only

--- namaste-icd11-backend/backend/src/database/database.module.ts
+++ namaste-icd11-backend/backend/src/database/database.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { FhirRepository } from './fhir.repository';
-import { FhirResource, FhirResourceSchema } from './fhir-resource.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: FhirResource.name, schema: FhirResourceSchema },
-    ]),
-  ],
-  providers: [FhirRepository],
-  exports: [FhirRepository],
+  providers: [],
+  exports: [],
 })
 export class DatabaseModule {}
